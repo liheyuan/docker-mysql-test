38c4608a8a2974d348d31f29098186255e4d1849
coder4 <lihy@fenbi.com>
2017-08-24 17:34:00 +0800

    ADD: create_db.sh

---
+++ create_db.sh
@@ -0,0 +1,13 @@
+if [ $# -ne 1 ]; then
+    echo "Usage $0 <dbname>"
+    exit -1
+fi
+
+DB_NAME=$1
+DB_USER="sbmvt"
+DB_PASS="hehehe"
+
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARSET utf8 COLLATE utf8_general_ci;"
+echo "CREATE USER $DB_USER IDENTIFIED by '$DB_PASS';"
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* to $DB_USER"
+echo "FLUSH PRIVILEGES"
