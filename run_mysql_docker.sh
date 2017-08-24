59af87ee753d80f5fad0a8431f63c2d9ad968f6f
Coder4 <liheyuan@users.noreply.github.com>
2017-08-24 12:50:18 +0800

    Update README.md

--- run_mysql_docker.sh
+++ run_mysql_docker.sh
@@ -3,4 +3,6 @@ NODE_NAME="node-mysql"
 DOCKER_NAME="sbmvt-mysql"
 MYSQL_DATA="$HOME/docker_data/mysql"
 eval $(docker-machine env $NODE_NAME)
+last_id=$(docker ps -l -q)
+docker rm -f $last_id
 docker run --rm --name $DOCKER_NAME -v "$MYSQL_DATA":/var/lib/mysql -e MYSQL_ROOT_PASSWORD=hehehe -p3306:3306 -d mysql:5.7
