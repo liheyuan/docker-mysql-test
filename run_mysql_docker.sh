b55a2af99f3ec04e3d4bf35a9a412f0457067486
coder4 <lihy@fenbi.com>
2017-08-26 06:53:38 +0800

    MOD: Add static ip

--- run_mysql_docker.sh
+++ run_mysql_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 NODE_NAME="node-mysql"
 DOCKER_NAME="sbmvt-mysql"
-MYSQL_DATA="$HOME/docker_data/mysql"
+VOLUME_DATA='/var/lib/boot2docker/docker_data/mysql'
 eval $(docker-machine env $NODE_NAME)
 last_id=$(docker ps -l -q)
 docker rm -f $last_id
-docker run --rm --name $DOCKER_NAME -v "$MYSQL_DATA":/var/lib/mysql -e MYSQL_ROOT_PASSWORD=hehehe -p3306:3306 -d mysql:5.7
+docker run --rm --name $DOCKER_NAME -v "$VOLUME_DATA":/var/lib/mysql -e MYSQL_ROOT_PASSWORD=hehehe -p3306:3306 -d mysql:5.7
